b03c9864a73892dcf81703cda990375ca8ee49c0
Swięcki Michał <kol19pl@gmail.com>
2019-10-16 11:13:48 +0200

    Dodaj pliki projektów.

---
+++ Android resurce renamer/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Android_resurce_renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Folder wejściowy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Folder wyjściowy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Enkoduj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(183, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(73, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Liczba plików do przetworzenia:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(314, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Dekryptuj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 202);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Android resurce renamer kol v1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Android resurce renamer/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Android_resurce_renamer
+{
+    public partial class Form1 : Form
+    {
+        String folderWejsciowy;
+        String folderZapisu;
+        string[] listaplikow;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            folderWejsciowy = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialogfolderwejsciowy = new FolderBrowserDialog();
+            if (dialogfolderwejsciowy.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderWejsciowy = dialogfolderwejsciowy.SelectedPath;
+                textBox1.Text = folderWejsciowy;
+                Odczytplików();
+              //  MessageBox.Show(folderWejsciowy+"\n"+"Liczba plików: " +listaplikow.Length);
+                textBox3.Text = listaplikow.Length.ToString();
+               
+
+            }
+
+
+          //  OpenFileDialog openFileDialog1 = new OpenFileDialog();
+          //  if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+          //  {
+          //      folderWejsciowy = openFileDialog1.FileName;
+          //      MessageBox.Show(folderWejsciowy);
+          //
+          //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialogfolderwejsciowy = new FolderBrowserDialog();
+            if (dialogfolderwejsciowy.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderZapisu = dialogfolderwejsciowy.SelectedPath;
+                textBox2.Text = folderZapisu;
+             //   MessageBox.Show(folderZapisu);
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderWejsciowy == null)
+            {
+                MessageBox.Show("Brak lokalizacji odczytu");
+            }
+            if (folderZapisu == null)
+            {
+                MessageBox.Show("Brak lokalizacji zapisu");
+            }
+            if (folderZapisu != null && folderWejsciowy != null)
+            {
+              //  MessageBox.Show("Procedura w toku");
+                Odczytplików();
+                Zapisplików();
+            }
+            
+
+            
+        }
+
+        void Odczytplików()
+        {
+            listaplikow = Directory.GetFiles(folderWejsciowy);
+           
+        }
+        void Zapisplików()
+        {
+            foreach (string f in listaplikow)
+            {
+                // Remove path from the file name.
+                string fName = f.Substring(folderWejsciowy.Length + 1);
+                string Operator = fName;
+
+                Operator = Operator.Replace(".mp3", "");
+                Operator = Operator.ToLower();
+                Operator = Operator.Replace("ć", "c");
+                Operator = Operator.Replace(" ", "_");
+                Operator = Operator.Replace("1", "_");
+                Operator = Operator.Replace("0", "_");
+                Operator = Operator.Replace("2", "_");
+                Operator = Operator.Replace("3", "_");
+                Operator = Operator.Replace("4", "_");
+                Operator = Operator.Replace("5", "_");
+                Operator = Operator.Replace("6", "_");
+                Operator = Operator.Replace("7", "_");
+                Operator = Operator.Replace("8", "_");
+                Operator = Operator.Replace("9", "_");
+                Operator = Operator.Replace("-", "_");
+                Operator = Operator.Replace("(", "_");
+                Operator = Operator.Replace(")", "_");
+                Operator = Operator.Replace(">", "_");
+                Operator = Operator.Replace("<", "_");
+                Operator = Operator.Replace("+", "_");
+                Operator = Operator.Replace("ą", "aa");
+                Operator = Operator.Replace("ć", "cc");
+                Operator = Operator.Replace("ę", "ee");
+                Operator = Operator.Replace("ó", "uu");
+                Operator = Operator.Replace("ś", "ss");
+                Operator = Operator.Replace("ż", "zzz");
+                Operator = Operator.Replace("ł", "ll");
+                Operator = Operator.Replace("ń", "nn");
+
+                string fNwename = Operator+".mp3";
+
+                // Use the Path.Combine method to safely append the file name to the path.
+                // Will overwrite if the destination file already exists.
+                File.Copy(Path.Combine(folderWejsciowy, fName), Path.Combine(folderZapisu, fNwename), true);
+                
+            }
+            Spisplikuw();
+        }
+        void Spisplikuw()
+        {
+           // File.Create(folderZapisu + "\\update.txt");
+            String content="Android ranamer v1    " + System.DateTime.Now + "\n";
+            foreach (string f in listaplikow)
+            {
+                content = content + f.Substring(folderWejsciowy.Length + 1) + "\n";
+            }
+            content = content + "end////////////////////////////end";
+            File.WriteAllText(folderZapisu+ "\\update.txt", content,Encoding.UTF8);
+            MessageBox.Show("plik update dopisany");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (string f in listaplikow)
+            {
+                // Remove path from the file name.
+                string fName = f.Substring(folderWejsciowy.Length + 1);
+                string Operator = fName;
+
+                Operator = Operator.Replace(".mp3", "");
+                Operator = Operator.ToLower();
+                Operator = Operator.Replace("_", " ");
+                Operator = Operator.Replace("aa","ą");
+                Operator = Operator.Replace("cc","ć");
+                Operator = Operator.Replace("ee","ę");
+                Operator = Operator.Replace("uu","ó");
+                Operator = Operator.Replace("ss","ś");
+                Operator = Operator.Replace("zzz","ż");
+                Operator = Operator.Replace("ll","ł");
+                Operator = Operator.Replace("nn", "ń");
+
+                string fNwename = Operator + ".mp3";
+
+                // Use the Path.Combine method to safely append the file name to the path.
+                // Will overwrite if the destination file already exists.
+                File.Copy(Path.Combine(folderWejsciowy, fName), Path.Combine(folderZapisu, fNwename), true);
+
+            }
+            Spisplikuw();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
